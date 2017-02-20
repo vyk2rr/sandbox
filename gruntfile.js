1d727743d44521c44aa8841f4a6a1b21453f0849
Victor De la Rocha <victor.delarocha@magmalabs.io>
2017-02-20 12:31:39 -0600

    Adds SASS bourbon and basic structure of SASS Files

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'public/stylesheets/style.css' : 'sass/style.scss'
+          'public/stylesheets/style.css' : 'sass/application.scss'
         }
       }
     },
